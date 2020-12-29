7fa1eb922af95dd02d9f040d2d2ae06f3ace0dea
Alexandr Rudenko <alexandr.rudenko@protonmail.com>
2020-12-29 19:16:39 +0100

    Add nested route and Nest component

--- router-test/src/Nest.js
+++ router-test/src/Nest.js
@@ -1,21 +1,23 @@
 import { Link } from "react-router-dom";
 import React from "react";
 
-function App(props) {
+function Nest(props) {
+  console.log(props);
   return (
-    <div className="App">
-      <h1>Hello from App</h1>
-      <h3>Count is: {props.count}</h3>
+    <div className="Nest">
+      <h1>W E L C O M E TO THE N E S T </h1>
+      {/* <h3>Count is: {props.count}</h3>
       <button type="button" onClick={props.handleClick}>
         Increment
       </button>
       <br></br>
       <br></br>
-      <br></br>
+      <br></br> */}
+      <p>Btw count is {props.count}</p>
 
       <Link to="/profile">Go to Profile page</Link>
     </div>
   );
 }
 
-export default App;
+export default Nest;
--- router-test/src/Profile.js
+++ router-test/src/Profile.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import Nest from "./Nest";
 
-const Profile = () => {
+const Profile = (props) => {
+  let { path, url } = useRouteMatch();
+  let { count, setCount } = props;
+
+  console.log(path);
+  console.log(url);
   return (
     <div>
       <h1>Hello from Profile</h1>
-
+      <h3>Count is now: {props.count}</h3>
+      <button type="button" onClick={props.handleClick}>
+        Increment
+      </button>
+      <br></br>
+      <br></br>
+      <br></br>
       <Link to="/">Go to App page</Link>
+
+      <br></br>
+      <Link to={`${url}/Nest`}>Take me to the nest</Link>
+
+      <Switch>
+        <Route
+          path={`${path}/Nest`}
+          render={(props) => (
+            <Nest {...props} count={count} setCount={setCount} />
+          )}
+        />
+      </Switch>
     </div>
   );
 };
